c7520bc3d8bd2b8478686be3842e780eef81cacc
Tim Dikland <tim.dikland@databricks.com>
2024-01-16 11:26:03 +0100

    extra test

--- test/test_example.py
+++ test/test_example.py
@@ -1,5 +1,8 @@
 import pytest
 
+import pyspark.testing
+from pyspark.testing.utils import assertDataFrameEqual
+
 def test_it_works():
     assert 1 == 1
 
@@ -8,3 +11,9 @@ def test_another():
 
 def test_more():
     assert 2 + 2 == 4
+
+def test_f(spark_fixture):
+    df_in = spark_fixture.read.csv("/Users/Tim.Dikland/customers/sse/azure-devops-pipeline-demo/test/fixtures/names.csv", header=True)
+    df_in.show()
+    assert 0
+
